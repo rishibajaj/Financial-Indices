e44890705d0f5af5c873b5388064f36b768c1e99
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-02-10 20:10:57 +0000

    Created nyt function and the function to construct the nyt api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,40 @@ let formatter = new Intl.NumberFormat('en-US');
 let URL = "https://api.coingecko.com/api/v3/coins/"
 
 
+// In the construction of the buildQueryURL I have follow procedure used in class to construct the NYT search app.
+function buildQueryURL(result) {
+    // constructiong queryURL (URL of API)
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+    // Make an object containing API with the parameters to query: api-key, q (search term), begin_date,
+    // end_date, ... (we willnot be using begin or end date)
+    var queryPrmts = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
+
+    // Get the search input and add to the object being constructed
+    queryPrmts.q = result[0];
+
+    // console.log API URL constructed for troubleshooting
+    console.log("-------API URL--------\nURL: " + queryURL + $.param(queryPrmts) + "\n----------------------");
+    // Return the URL of the query
+    var query = queryURL + $.param(queryPrmts);
+    return query;
+}
+
+
+function nyt(result) {
+    // Obtain the NYT url calling the build function
+    var queryURL = buildQueryURL(result);
+    // Creating an AJAX call for the NYT API
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+		console.log("NYT response:", response);
+        //displayArticles(response);
+    });
+}
+
+
 function populateTable(result) {
 	let response = result[0];
 
@@ -36,6 +70,7 @@ function fetchTableData() {
 	.then((result) => {
 	  console.log(result);
 	  populateTable(result);
+	  nyt(result);
 	})
 	.catch(function (error) {
 	  console.log(error);
